fa616b04abf409f9537b97540bc891ea4fdfd22d
shimokei53 <respect.assistance.may.313@gmail.com>
2020-11-01 15:10:16 +0900

    https://book.cakephp.org/4/ja/tutorials-and-examples/cms/database.html

---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Model\Table;
+
+
+use Cake\ORM\Table;
+
+class ArticlesTable extends Table
+{
+    public function initialize(array $config): void
+    {
+        $this->addBehavior('Timestamp');
+    }
+}
